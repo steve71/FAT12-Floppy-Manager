20a3d6145dce4cbf47b37f7f7328ac206291b380
steve71 <steve028@yahoo.com>
2026-02-05 18:15:30 -0500

    added QLabel

--- gui_components.py
+++ gui_components.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (
     QDialog, QVBoxLayout, QTableWidget, QTableWidgetItem, 
-    QTabWidget, QHeaderView, QPushButton
+    QTabWidget, QHeaderView, QPushButton, QLabel
 )
 from PyQt6.QtCore import Qt
 
